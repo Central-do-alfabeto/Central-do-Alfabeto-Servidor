c19cd10b95801ec2b7df865dfcd3b557ebcdc95f
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-08-31 16:39:32 -0300

    Create EducadorRepository.java

---
+++ src/main/java/com/centraldoalfabeto/game/repository/EducadorRepository.java
@@ -0,0 +1,7 @@
+package com.centraldoalfabeto.game.repository;
+
+import com.centraldoalfabeto.game.domain.model.Educador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EducadorRepository extends JpaRepository<Educador, Long> {
+}
