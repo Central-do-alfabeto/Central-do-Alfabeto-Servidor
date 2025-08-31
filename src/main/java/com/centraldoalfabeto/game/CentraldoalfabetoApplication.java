bd2fd2d018a2d58be5f78fa26715bf386457033b
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-08-31 17:31:45 -0300

    Update and rename CentralDoAlfabetoApplication.java to CentraldoalfabetoApplication.java

--- src/main/java/com/centraldoalfabeto/game/CentraldoalfabetoApplication.java
+++ src/main/java/com/centraldoalfabeto/game/CentraldoalfabetoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CentralDoAlfabetoApplication {
+public class CentraldoalfabetoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CentralDoAlfabetoApplication.class, args);
+		SpringApplication.run(CentraldoalfabetoApplication.class, args);
 	}
 }
