623ead921d87b1599d0cfa5f0d92c892bbbc4acc
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-01 20:08:02 -0300

    Update JogadorRepository.java

--- src/main/java/com/centraldoalfabeto/game/repository/JogadorRepository.java
+++ src/main/java/com/centraldoalfabeto/game/repository/JogadorRepository.java
@@ -3,5 +3,8 @@ package com.centraldoalfabeto.game.repository;
 import com.centraldoalfabeto.game.domain.model.Jogador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JogadorRepository extends JpaRepository<Jogador, Long> {
+	Optional<Jogador> findByUsername(String username);
 }
