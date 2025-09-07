ee651324098679726b38a27b853601c625a4635b
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-07 19:11:57 -0300

    Update EducadorRepository.java

--- src/main/java/com/centraldoalfabeto/game/repository/EducadorRepository.java
+++ src/main/java/com/centraldoalfabeto/game/repository/EducadorRepository.java
@@ -2,6 +2,8 @@ package com.centraldoalfabeto.game.repository;
 
 import com.centraldoalfabeto.game.domain.model.Educador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface EducadorRepository extends JpaRepository<Educador, Long> {
+    Optional<Educador> findByEmail(String email);
 }
