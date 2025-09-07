9b5880101c728acccdb396675d88b0ba1830dba6
Luiz Henrique Freitas <166095630+LuizHenriqueFreitas@users.noreply.github.com>
2025-09-06 23:26:32 -0300

    SUPABASE IMPLEMENTATION

--- src/main/java/com/centraldoalfabeto/game/repository/JogadorRepository.java
+++ src/main/java/com/centraldoalfabeto/game/repository/JogadorRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface JogadorRepository extends JpaRepository<Jogador, Long> {
-	Optional<Jogador> findByUsername(String username);
+	Optional<Jogador> findByFullName(String full_name);
 }
