f9129e3bab070395c74833a9a657eefaec4741f9
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-07 19:12:48 -0300

    Update Jogador.java

--- src/main/java/com/centraldoalfabeto/game/domain/model/Jogador.java
+++ src/main/java/com/centraldoalfabeto/game/domain/model/Jogador.java
@@ -1,9 +1,13 @@
 package com.centraldoalfabeto.game.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "players")
+@Data
+@NoArgsConstructor
 public class Jogador {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,71 +18,26 @@ public class Jogador {
     private String password;
 
     private Integer currentPhaseIndex;
+    
+    @Transient
+    private Integer numberOfErrors;
+    
+    @Transient
+    private Integer numberOfSoundRepeats;
 
     @ElementCollection
     private int[] numberOfErrorsByPhase;
 
     @ElementCollection
     private int[] numberOfSoundRepeatsByPhase;
-
-    public Jogador() {
-        this.numberOfErrorsByPhase = new int[50];
-        this.numberOfSoundRepeatsByPhase = new int[50];
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getCurrentPhaseIndex() {
-        return currentPhaseIndex;
-    }
-
-    public void setCurrentPhaseIndex(Integer currentPhaseIndex) {
-        this.currentPhaseIndex = currentPhaseIndex;
-    }
-
-    public int[] getNumberOfErrorsByPhase() {
-        return numberOfErrorsByPhase;
-    }
-
-    public void setNumberOfErrorsByPhase(int[] numberOfErrorsByPhase) {
-        this.numberOfErrorsByPhase = numberOfErrorsByPhase;
-    }
-
-    public int[] getNumberOfSoundRepeatsByPhase() {
-        return numberOfSoundRepeatsByPhase;
-    }
-
-    public void setNumberOfSoundRepeatsByPhase(int[] numberOfSoundRepeatsByPhase) {
-        this.numberOfSoundRepeatsByPhase = numberOfSoundRepeatsByPhase;
+    
+    @PostLoad
+    private void initializeArraysIfNull() {
+        if (this.numberOfErrorsByPhase == null) {
+            this.numberOfErrorsByPhase = new int[10];
+        }
+        if (this.numberOfSoundRepeatsByPhase == null) {
+            this.numberOfSoundRepeatsByPhase = new int[10];
+        }
     }
 }
