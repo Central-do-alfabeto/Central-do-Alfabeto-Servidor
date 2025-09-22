8fa40bc3bcabfd0371ec4563735ed52cc143524f
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-22 20:03:50 -0300

    Update UnifiedLoginResponseDTO.java

--- src/main/java/com/centraldoalfabeto/game/dto/UnifiedLoginResponseDTO.java
+++ src/main/java/com/centraldoalfabeto/game/dto/UnifiedLoginResponseDTO.java
@@ -1,17 +1,26 @@
 package com.centraldoalfabeto.game.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.Map;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class UnifiedLoginResponseDTO {
     private Long userId;
     private boolean isStudent;
     private Integer currentPhaseIndex;
     private Map<Long, String> students;
+
+    public UnifiedLoginResponseDTO(Long userId, boolean isStudent, Integer currentPhaseIndex) {
+        this.userId = userId;
+        this.isStudent = isStudent;
+        this.currentPhaseIndex = currentPhaseIndex;
+    }
+
+    public UnifiedLoginResponseDTO(Long userId, boolean isStudent, Map<Long, String> students) {
+        this.userId = userId;
+        this.isStudent = isStudent;
+        this.students = students;
+    }
 }
