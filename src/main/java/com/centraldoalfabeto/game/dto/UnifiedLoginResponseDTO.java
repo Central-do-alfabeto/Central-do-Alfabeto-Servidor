9bdb2524d2e863b702029612372c1c7e8887f9fc
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-22 19:58:18 -0300

    Update UnifiedLoginResponseDTO.java

--- src/main/java/com/centraldoalfabeto/game/dto/UnifiedLoginResponseDTO.java
+++ src/main/java/com/centraldoalfabeto/game/dto/UnifiedLoginResponseDTO.java
@@ -1,9 +1,14 @@
 package com.centraldoalfabeto.game.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UnifiedLoginResponseDTO {
     private Long userId;
     private boolean isStudent;
