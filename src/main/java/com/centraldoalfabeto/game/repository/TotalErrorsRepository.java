59e23d4130ce8f0387bbf3fa6a162033b9d1974d
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-22 19:34:51 -0300

    Create TotalErrorsRepository.java

---
+++ src/main/java/com/centraldoalfabeto/game/repository/TotalErrorsRepository.java
@@ -0,0 +1,12 @@
+package com.centraldoalfabeto.game.repository;
+
+import com.centraldoalfabeto.game.domain.model.TotalErrors;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TotalErrorsRepository extends JpaRepository<TotalErrors, Long> {
+    List<TotalErrors> findByPlayersId(Long playersId);
+}
