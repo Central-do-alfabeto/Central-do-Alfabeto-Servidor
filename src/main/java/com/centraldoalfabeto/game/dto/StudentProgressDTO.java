a420951fb111c90fba2896f084754659ce9c7979
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-22 19:40:38 -0300

    Update StudentProgressDTO.java

--- src/main/java/com/centraldoalfabeto/game/dto/StudentProgressDTO.java
+++ src/main/java/com/centraldoalfabeto/game/dto/StudentProgressDTO.java
@@ -1,10 +1,11 @@
 package com.centraldoalfabeto.game.dto;
 
 import lombok.Data;
+import java.util.Map;
 
 @Data
 public class StudentProgressDTO {
     private Integer currentPhaseIndex;
-    private Integer[] numberOfErrorsByPhase;
-    private Integer[] numberOfSoundRepeatsByPhase;
+    private Map<Integer, Integer> numberOfErrorsByPhase;
+    private Map<Integer, Integer> numberOfSoundRepeatsByPhase;
 }
