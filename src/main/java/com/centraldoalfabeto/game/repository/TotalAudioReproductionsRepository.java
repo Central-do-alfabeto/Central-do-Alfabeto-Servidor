4d49943d094e80d20519c9eee1e2f15e7c0b589e
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-22 19:35:15 -0300

    Create TotalAudioReproductionsRepository.java

---
+++ src/main/java/com/centraldoalfabeto/game/repository/TotalAudioReproductionsRepository.java
@@ -0,0 +1,12 @@
+package com.centraldoalfabeto.game.repository;
+
+import com.centraldoalfabeto.game.domain.model.TotalAudioReproductions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TotalAudioReproductionsRepository extends JpaRepository<TotalAudioReproductions, Long> {
+    List<TotalAudioReproductions> findByPlayersId(Long playersId);
+}
