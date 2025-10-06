126481c3570565b992eac3874492547a5213546c
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 16:14:06 -0300

    Update UnifiedLoginResponseDTO.java

--- src/main/java/com/centraldoalfabeto/game/dto/UnifiedLoginResponseDTO.java
+++ src/main/java/com/centraldoalfabeto/game/dto/UnifiedLoginResponseDTO.java
@@ -4,37 +4,49 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 public class UnifiedLoginResponseDTO {
-    private Long userId;
+    private UUID userId; 
 
     @JsonProperty("isStudent")
     private boolean student;
-    private Integer currentPhaseIndex;
+    
+    private Integer currentPhaseIndex; 
     
     private List<StudentSummaryDTO> studentSummaries;
     private String token;
 
-    public UnifiedLoginResponseDTO(Long userId, boolean isStudent, Integer currentPhaseIndex) {
+    public UnifiedLoginResponseDTO(UUID userId, boolean isStudent, Integer currentPhaseIndex) {
         this(userId, isStudent, currentPhaseIndex, null);
     }
 
-    public UnifiedLoginResponseDTO(Long userId, boolean isStudent, List<StudentSummaryDTO> studentSummaries) {
-        this(userId, isStudent, studentSummaries, null);
+    public UnifiedLoginResponseDTO(UUID userId, boolean isStudent, List<StudentSummaryDTO> studentSummaries) {
+        this(userId, isStudent, null, studentSummaries, null);
     }
-
-    public UnifiedLoginResponseDTO(Long userId, boolean isStudent, Integer currentPhaseIndex, String token) {
+    
+    public UnifiedLoginResponseDTO(UUID userId, boolean isStudent, Integer currentPhaseIndex, String token) {
         this.userId = userId;
         this.student = isStudent;
         this.currentPhaseIndex = currentPhaseIndex;
         this.token = token;
+        this.studentSummaries = null;
     }
 
-    public UnifiedLoginResponseDTO(Long userId, boolean isStudent, List<StudentSummaryDTO> studentSummaries, String token) {
+    public UnifiedLoginResponseDTO(UUID userId, boolean isStudent, List<StudentSummaryDTO> studentSummaries, String token) {
         this.userId = userId;
         this.student = isStudent;
+        this.studentSummaries = studentSummaries;
+        this.token = token;
+        this.currentPhaseIndex = null;
+    }
+    
+    public UnifiedLoginResponseDTO(UUID userId, boolean isStudent, Integer currentPhaseIndex, List<StudentSummaryDTO> studentSummaries, String token) {
+        this.userId = userId;
+        this.student = isStudent;
+        this.currentPhaseIndex = currentPhaseIndex;
         this.studentSummaries = studentSummaries;
         this.token = token;
     }
