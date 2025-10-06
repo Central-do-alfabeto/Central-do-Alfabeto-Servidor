959a5211a93d9d9b60e528bba6acf3ef4e8290df
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 16:43:51 -0300

    Update PlayersDataRepository.java

--- src/main/java/com/centraldoalfabeto/game/repository/PlayersDataRepository.java
+++ src/main/java/com/centraldoalfabeto/game/repository/PlayersDataRepository.java
@@ -3,6 +3,8 @@ package com.centraldoalfabeto.game.repository;
 import com.centraldoalfabeto.game.domain.model.PlayersData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
