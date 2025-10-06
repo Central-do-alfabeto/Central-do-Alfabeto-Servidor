44dcf5191d4e4e79fcbe0446bbceaa030df53c05
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 15:08:42 -0300

    Update JogadorRepository.java

--- src/main/java/com/centraldoalfabeto/game/repository/JogadorRepository.java
+++ src/main/java/com/centraldoalfabeto/game/repository/JogadorRepository.java
@@ -2,11 +2,16 @@ package com.centraldoalfabeto.game.repository;
 
 import com.centraldoalfabeto.game.domain.model.Jogador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.UUID;
 import java.util.Optional;
 
-public interface JogadorRepository extends JpaRepository<Jogador, Long> {
-    Optional<Jogador> findByFullName(String fullName);
+public interface JogadorRepository extends JpaRepository<Jogador, UUID> {
+    @Query("SELECT e FROM Jogador e JOIN e.user u WHERE u.nome = :fullName")
+    Optional<Jogador> findByFullName(@Param("fullName") String fullName);
     
-    Optional<Jogador> findByEmail(String email);
+    @Query("SELECT e FROM Jogador e JOIN e.user u WHERE u.email = :email")
+    Optional<Jogador> findByEmail(@Param("email") String email);
 }
