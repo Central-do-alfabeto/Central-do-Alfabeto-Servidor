2bfe356fb834013186387e2759c1b25e591116c0
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 16:20:48 -0300

    Create UserRepository.java

---
+++ src/main/java/com/centraldoalfabeto/game/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.centraldoalfabeto.game.repository;
+
+import com.centraldoalfabeto.game.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
