5e53abcdfb496001f4a166bc723e513be00f3fab
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 15:00:14 -0300

    Update EducadorRepository.java

--- src/main/java/com/centraldoalfabeto/game/repository/EducadorRepository.java
+++ src/main/java/com/centraldoalfabeto/game/repository/EducadorRepository.java
@@ -2,8 +2,13 @@ package com.centraldoalfabeto.game.repository;
 
 import com.centraldoalfabeto.game.domain.model.Educador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.UUID; 
 import java.util.Optional;
 
-public interface EducadorRepository extends JpaRepository<Educador, Long> {
-    Optional<Educador> findByEmail(String email);
+public interface EducadorRepository extends JpaRepository<Educador, UUID> {
+    @Query("SELECT e FROM Educador e JOIN e.user u WHERE u.email = :email")
+    Optional<Educador> findByEmail(@Param("email") String email);
 }
