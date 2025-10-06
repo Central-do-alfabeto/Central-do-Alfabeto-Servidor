8c621de2d5ab70cfd8d51a91443b57e396b043e7
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 16:22:01 -0300

    Create EducatorRegistrationDTO.java

---
+++ src/main/java/com/centraldoalfabeto/game/dto/EducatorRegistrationDTO.java
@@ -0,0 +1,12 @@
+package com.centraldoalfabeto.game.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EducatorRegistrationDTO {
+    private String nome;
+    private String email;
+    private String senha;
+}
