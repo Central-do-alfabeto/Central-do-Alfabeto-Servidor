da1aed3a6d10830faeac9276eb16344d1f235957
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 16:05:08 -0300

    Create ProgressService.java

---
+++ src/main/java/com/centraldoalfabeto/game/service/ProgressService.java
@@ -0,0 +1,50 @@
+package com.centraldoalfabeto.game.service;
+
+import com.centraldoalfabeto.game.domain.model.PlayersData;
+import com.centraldoalfabeto.game.dto.ProgressUpdateDTO;
+import com.centraldoalfabeto.game.repository.PlayersDataRepository;
+import com.centraldoalfabeto.game.security.JwtAuthenticatedUser;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.Optional;
+
+@Service
+public class ProgressService {
+    private final PlayersDataRepository playersDataRepository;
+
+    @Autowired
+    public ProgressService(PlayersDataRepository playersDataRepository) {
+        this.playersDataRepository = playersDataRepository;
+    }
+
+    @Transactional
+    public PlayersData updateProgress(JwtAuthenticatedUser authenticatedUser, ProgressUpdateDTO dto) {
+        UUID playerId = authenticatedUser.getUserId();
+        
+        Optional<PlayersData> existingData = playersDataRepository.findById(playerId);
+        
+        PlayersData playerData = existingData.orElseGet(() -> {
+            PlayersData newPlayersData = new PlayersData();
+            newPlayersData.setPlayersId(playerId);
+          
+            return newPlayersData;
+        });
+        
+        if (dto.getCurrentPhaseIndex() != null) {
+             playerData.setPhaseIndex(dto.getCurrentPhaseIndex());
+        }
+        
+        if (dto.getErrorsData() != null) {
+            playerData.setErrosTotais(dto.getErrorsData());
+        }
+        
+        if (dto.getSoundRepeatsData() != null) {
+            playerData.setAudiosTotais(dto.getSoundRepeatsData());
+        }
+
+        return playersDataRepository.save(playerData);
+    }
+}
