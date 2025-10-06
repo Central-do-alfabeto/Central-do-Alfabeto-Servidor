f20330f2cfc15832073867730420a4f472bd9528
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 15:16:55 -0300

    Create PlayersDataRepository.java

---
+++ src/main/java/com/centraldoalfabeto/game/repository/PlayersDataRepository.java
@@ -0,0 +1,11 @@
+package com.centraldoalfabeto.game.repository;
+
+import com.centraldoalfabeto.game.domain.model.PlayersData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface PlayersDataRepository extends JpaRepository<PlayersData, UUID> {
+    Optional<PlayersData> findByPlayersId(UUID playerId);
+}
