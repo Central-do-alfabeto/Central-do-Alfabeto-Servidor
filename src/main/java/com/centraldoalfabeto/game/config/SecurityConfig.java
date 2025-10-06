4ccf834ad4a326e32e25f59b6319f6bce41613e6
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 14:53:11 -0300

    Update SecurityConfig.java

--- src/main/java/com/centraldoalfabeto/game/config/SecurityConfig.java
+++ src/main/java/com/centraldoalfabeto/game/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 public class SecurityConfig {
-
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
