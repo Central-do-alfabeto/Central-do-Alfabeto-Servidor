8b29dadf28321bdc2713b2bc80807120381fd5ff
brian-gustavo <brian20062005@gmail.com>
2025-11-09 20:42:42 -0300

    tentando resolver o Vosk

--- src/test/java/com/centraldoalfabeto/game/CentralDoAlfabetoApplicationTests.java
+++ src/test/java/com/centraldoalfabeto/game/CentralDoAlfabetoApplicationTests.java
@@ -2,10 +2,15 @@ package com.centraldoalfabeto.game;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+import com.centraldoalfabeto.game.service.VoskTranscriptionService;
 
 @SpringBootTest
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = VoskTranscriptionService.class))
 class CentralDoAlfabetoApplicationTests {
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 }
